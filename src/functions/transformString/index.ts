b63c73776e8f52283786bb044295d8b9a0df495b
Sami Armando Muongueno Sabino <samisabino@gmail.com>
2023-12-20 01:50:29 +0100

    Update Transform Method

--- src/functions/transformString/index.ts
+++ src/functions/transformString/index.ts
@@ -1,7 +1,7 @@
 /* eslint-disable prettier/prettier */
 export function transform(name: string) {
 
-    return name.trim().split(' ').map(word => {
+    return name.toLowerCase().trim().split(' ').map(word => {
         return word[0].toLocaleUpperCase().concat(word.substring(1))
     }).join(' ')
 }
